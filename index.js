ff3ed965b4e067618e307de8e8ca8bd822199f6f
Rita Law <rita.law86@gmail.com>
2019-03-19 23:38:53 -0400

    working assets being served

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
+const nunjucks = require("nunjucks");
+const sass = require("node-sass");
+const fs = require("fs");
 
 app.use(helmet.noSniff());
-app.use(helmet.frameGuard({action:"deny"}));
+app.use(helmet.frameguard({action:"deny"}));
 app.use(helmet.xssFilter());
 
+
+let env= new nunjucks.Environment(new nunjucks.FileSystemLoader("views"));
+
+env.express(app);
+
 const port =process.env.PORT || 3000;
+
 app.listen(port, (req, res)=>{
    console.log("Now listening on port "+ port);
+});
+
+sass.render({
+  file: "./scss/main.scss"
+}, (err, result)=>{
+ if (err){
+   console.log("Unable to render scss file. Error :" + err);
+   return;
+ }
+ fs.writeFile('./css/output.css', result.css, (err)=>{
+   if (err){
+     console.log("Error writing file to disk: " + err);
+     return;
+   }
+ });
+});
+
+app.use(express.static('css'));
+
+app.get("/", (req, res)=>{
+ res.render("home.html")
 });
