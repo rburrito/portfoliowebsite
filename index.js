a901cb60ca8298af13165ed5cce393c1c635d565
Rita Law <rita.law86@gmail.com>
2019-04-01 16:48:51 -0400

    new projects added

--- index.js
+++ index.js
@@ -42,17 +42,29 @@ sass.render({
 app.use(express.static('public'));
 
 app.get("/", (req, res)=>{
-  res.render("home.html")
+  res.render("home.html", {title: "Home"});
 });
 
 app.get("/about", (req, res)=>{
-  res.render("about.html")
+  res.render("about.html", {title: "About"});
 });
 
 app.get("/projects", (req, res)=>{
-  res.render("projects.html")
+  res.render("projects.html", {title: "Projects"});
+});
+
+app.get("/ticTacToe", (req, res)=>{
+  res.render("ticTacToe.html", {title: "Tic Tac Toe"});
 });
 
 app.get("/localweather", (req, res)=>{
-  res.render("localweather.html")
+  res.render("localweather.html", {title: "Local Weather API"});
 });
+
+app.get("/pomodoro", (req, res)=>{
+  res.render("pomodoroClock.html", {title: "Fabulous Pomodoro Clock"});
+});
+
+app.get("/simon", (req, res)=>{
+  res.render("simon.html", {title: "Simon Game"});
+})
