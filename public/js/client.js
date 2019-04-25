197ac080073cc450dde2fd14519e3b1192aa0916
Rita Law <rita.law86@gmail.com>
2019-04-25 16:03:48 -0400

    changed grammatical error

--- public/js/client.js
+++ public/js/client.js
@@ -3,24 +3,27 @@ $( document ).ready(function() {
   var socket = io();
   // listens for the user event and data object sent
   socket.on('user', function(data){
-  console.log(data);  
-    
-    $('#num-users').text(data.currentUsers+' users online');
+  console.log(data);
+    let users = "user";
+    if (data.currentUsers>1){
+      users = users +s;
+    }
+    $('#num-users').text(data.currentUsers+ ' '+ users + ' online');
     var message = data.name;
     if (data.connected){
-    message+=' has joined the chat.'; 
+    message+=' has joined the chat.';
     } else {
     message+=' has left the chat.';
     }
     $('#messages').append($('<li>').html('<b>'+message+'</b></li>'));
-  
+
   });
-  
+
   // disconnects from chat
   socket.on('disconnect', function(){
   });
-   
- 
+
+
   // Form submittion with new message in field with id 'm'
   $('form').submit(function(){
     var messageToSend = $('#m').val();
@@ -29,11 +32,11 @@ $( document ).ready(function() {
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
-  
-  
+
+
   socket.on('chat message', (message)=>{
   $('#messages').append($('<li>').html('<b>'+message.name+': '+message.message+'</b></li>'));
   });
-  
-  
+
+
 });
